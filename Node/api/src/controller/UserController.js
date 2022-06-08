ef2170c575d95a1e3b17e4cc52d76a1cff95339b
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-08 09:25:19 -0300

    Add query params

--- Node/api/src/controller/UserController.js
+++ Node/api/src/controller/UserController.js
@@ -2,6 +2,17 @@ const users = require('../mocks/users');
 
 module.exports = {
   listUsers(request, response) {
+    const { order } = request.query;
+
+    console.log();
+
+    const sortedUsers = users.sort((a, b) => {
+      if (order === 'desc') {
+        return a.id < b.id ? 1 : -1;
+      }
+      return a.id > b.id ? 1 : -1;
+    });
+
     response.writeHead(200, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(users)); // .end sempre espera uma string
   },
