a4ec8fd4d8ab1dc893526e1657a1f2e0ced78bbd
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-13 23:29:25 -0300

    Refactoring in queries

--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -4,13 +4,25 @@ class ContactsRepository {
   async findAll(orderBy = 'ASC') {
     const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
     const rows = await db.query(
-      `SELECT * FROM contacts ORDER BY name ${direction}`,
+      `
+      SELECT contacts.*, categories.name AS category_name
+      FROM contacts
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      ORDER BY contacts.name ${direction}
+      `,
     );
     return rows;
   }
 
   async findById(id) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    const [row] = await db.query(
+      `
+      SELECT contacts.*, categories.name AS category_name
+      FROM contacts
+      LEFT JOIN categories ON categories.id = contacts.category_id
+      WHERE contacts.id = $1`,
+      [id],
+    );
     return row;
   }
 
@@ -24,23 +36,29 @@ class ContactsRepository {
   async create({
     name, email, phone, category_id,
   }) {
-    const [row] = await db.query(`
+    const [row] = await db.query(
+      `
       INSERT INTO contacts(name, email, phone, category_id)
       VALUES($1, $2, $3, $4)
       RETURNING *
-      `, [name, email, phone, category_id]);
+      `,
+      [name, email, phone, category_id],
+    );
     return row;
   }
 
   async update(id, {
     name, email, phone, category_id,
   }) {
-    const [row] = await db.query(`
+    const [row] = await db.query(
+      `
       UPDATE contacts
       SET name = $1, email = $2, phone = $3, category_id = $4
       WHERE id = $5
       RETURNING *
-    `, [name, email, phone, category_id, id]);
+    `,
+      [name, email, phone, category_id, id],
+    );
     return row;
   }
 
