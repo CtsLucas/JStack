d9c542033ffcf3fc3092c71617f72cced3f8c0fc
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-20 23:41:47 -0300

    Config Babel

---
+++ react-fundamentos/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _user$addres;
+
+var user = {};
+console.log(user === null || user === void 0 ? void 0 : (_user$addres = user.addres) === null || _user$addres === void 0 ? void 0 : _user$addres.street);
