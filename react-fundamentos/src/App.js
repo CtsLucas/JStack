2027a2d004434488a8d0c89d81088cdcbf5966dd
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-21 22:04:47 -0300

    feat: :sparkles: Add Props

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import Post from './Post';
 
 export default function App() {
-  return <h1>Componente App</h1>;
+  return (
+    <>
+      <h1>JStack's Blog</h1>
+      <h2>Posts da semana</h2>
+      <hr />
+
+      <Post
+        post={{
+          title: 'Título da notícia 01',
+          subtitle: 'Subtítulo da notícia 01',
+        }}
+      />
+      <Post
+        post={{
+          title: 'Título da notícia 02',
+          subtitle: 'Subtítulo da notícia 02',
+        }}
+      />
+      <Post
+        post={{
+          title: 'Título da notícia 03',
+          subtitle: 'Subtítulo da notícia 03',
+        }}
+      />
+    </>
+  );
 }
