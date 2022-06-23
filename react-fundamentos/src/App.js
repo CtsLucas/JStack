ffcd6fe84588b1b486bd9546cc604802eb129317
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-23 19:44:45 -0300

    feat: :sparkles: Add PropTypes

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -5,7 +5,7 @@ import Post from './Post';
 export default function App() {
   return (
     <>
-      <Header title="JStack's Blog">
+      <Header>
         <h2>Posts da semana</h2>
       </Header>
       <hr />
@@ -14,18 +14,21 @@ export default function App() {
           title: 'Título da notícia 01',
           subtitle: 'Subtítulo da notícia 01',
         }}
+        likes={20}
       />
       <Post
         post={{
           title: 'Título da notícia 02',
           subtitle: 'Subtítulo da notícia 02',
         }}
+        likes={10}
       />
       <Post
         post={{
           title: 'Título da notícia 03',
           subtitle: 'Subtítulo da notícia 03',
         }}
+        likes={30}
       />
     </>
   );
--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Post(props) {
   return (
@@ -7,8 +8,18 @@ export default function Post(props) {
         <strong>{props.post.title}</strong>
         <br />
         <small>{props.post.subtitle}</small>
+        <br />
+        Likes: {props.likes}
       </article>
       <br />
     </>
   );
 }
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+  }).isRequired,
+  likes: PropTypes.number.isRequired,
+};
