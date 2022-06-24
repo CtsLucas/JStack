81602ce39ecae9546936dcb9d10c2d39ede22496
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-06-23 22:46:11 -0300

    feat: :sparkles: Add Remove Post

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -3,45 +3,52 @@ import Header from './Header';
 import Post from './Post';
 
 export default function App() {
-  const [posts, setPost] = useState([
+  const [posts, setPosts] = useState([
     {
       id: Math.random(),
       title: 'Título da notícia 01',
       subtitle: 'Subtítulo da notícia 01',
       likes: 40,
+      read: false,
     },
     {
       id: Math.random(),
       title: 'Título da notícia 02',
       subtitle: 'Subtítulo da notícia 02',
       likes: 10,
+      read: true,
     },
     {
       id: Math.random(),
       title: 'Título da notícia 03',
       subtitle: 'Subtítulo da notícia 03',
       likes: 30,
+      read: false,
     },
     {
       id: Math.random(),
       title: 'Título da notícia 04',
       subtitle: 'Subtítulo da notícia 04',
       likes: 20,
+      read: true,
     },
   ]);
 
   function handleRefresh() {
-    setTimeout(() => {
-      setPost((prevState) => [
-        ...prevState,
-        {
-          id: Math.random(),
-          title: `Titulo da notícia 0${prevState.length + 1}`,
-          subtitle: `Subtítulo da notícia 0${prevState.length + 1}`,
-          likes: Math.floor(Math.random() * 100),
-        },
-      ]);
-    }, 2000);
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Titulo da notícia 0${prevState.length + 1}`,
+        subtitle: `Subtítulo da notícia 0${prevState.length + 1}`,
+        likes: Math.floor(Math.random() * 100),
+        read: false,
+      },
+    ]);
+  }
+
+  function handleRemovePost(postId) {
+    setPosts((prevState) => prevState.filter((post) => post.id !== postId));
   }
 
   return (
@@ -53,14 +60,7 @@ export default function App() {
       <hr />
 
       {posts.map((post) => (
-        <Post
-          key={post.id}
-          post={{
-            title: post.title,
-            subtitle: post.subtitle,
-          }}
-          likes={post.likes}
-        />
+        <Post key={post.id} post={post} onRemove={handleRemovePost} />
       ))}
     </>
   );
--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -5,11 +5,12 @@ export default function Post(props) {
   return (
     <>
       <article>
-        <strong>{props.post.title}</strong>
+        <strong>{props.post.read ? <s>{props.post.title}</s> : props.post.title}</strong>
+        <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
         <br />
         <small>{props.post.subtitle}</small>
         <br />
-        Likes: {props.likes}
+        Likes: {props.post.likes}
       </article>
       <br />
     </>
@@ -18,8 +19,11 @@ export default function Post(props) {
 
 Post.propTypes = {
   post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    read: PropTypes.bool.isRequired,
   }).isRequired,
-  likes: PropTypes.number.isRequired,
+  onRemove: PropTypes.func.isRequired,
 };
