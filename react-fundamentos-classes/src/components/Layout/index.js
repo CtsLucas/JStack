e915967b4ffd5350f5038dd0a2d05b2f5ec29e02
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-07-04 19:40:38 -0300

    feat: :art: Add useTheme

--- react-fundamentos-classes/src/components/Layout/index.js
+++ react-fundamentos-classes/src/components/Layout/index.js
@@ -3,13 +3,30 @@ import React from 'react';
 import Header from '../Header';
 import PostsList from '../PostsList';
 import Footer from '../Footer';
+import { useTheme } from 'styled-components';
 
 export default function Layout() {
-
+  const theme = useTheme();
   return (
     <>
       <Header />
       <PostsList />
+      <div // Exemplo de utilização do useTheme();
+        style={{
+          backgroundColor: theme.postBackgroundColor,
+          display: 'flex',
+          flexDirection: 'column',
+          padding: theme.spacing.medium,
+          borderRadius: theme.borderRadius,
+          marginTop: theme.spacing.small,
+        }}>
+        <h2 style={{
+          margin: 0,
+        }}>useTheme</h2>
+        <small>
+          Qui exercitation voluptate aute qui nisi.
+        </small>
+      </div>
       <Footer />
     </>
   );
