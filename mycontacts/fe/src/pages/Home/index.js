e9a5de8952fdfc67c05f534a909b385f4664f001
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-07-06 19:10:03 -0300

    feat: :sparkles: Create Home Page and Refactor in ContactsList

--- mycontacts/fe/src/pages/Home/index.js
+++ mycontacts/fe/src/pages/Home/index.js
@@ -1,5 +1,74 @@
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <Container>
+
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>
+              Nome
+            </span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Lucas Cavalcante</strong>
+              <small>instagram</small>
+            </div>
+            <span>lucas@jstack.com.br</span>
+            <span>(11) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Isabella Ohana</strong>
+              <small>facebook</small>
+            </div>
+            <span>isabella@jstack.com.br</span>
+            <span>(11) 88888-8888</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
   );
 }
